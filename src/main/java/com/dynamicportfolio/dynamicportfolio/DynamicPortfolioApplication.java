e47026330fb8ecf3304032ecd1bd0f262abe4d4f
gautamnegi <gautam.negi@coviam.com>
2020-05-09 21:37:53 +0530

    changed URI.

--- src/main/java/com/dynamicportfolio/dynamicportfolio/DynamicPortfolioApplication.java
+++ src/main/java/com/dynamicportfolio/dynamicportfolio/DynamicPortfolioApplication.java
@@ -2,10 +2,8 @@ package com.dynamicportfolio.dynamicportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication
 public class DynamicPortfolioApplication {
 
   public static void main(String[] args) {
