353cc4ecb06050621be32321ece009c9af91b8d7
gautamnegi <gautam.negi@coviam.com>
2020-05-09 21:19:11 +0530

    excluded MongoAutoConfiguration.class and MongoDataAutoConfiguration.class

--- src/main/java/com/dynamicportfolio/dynamicportfolio/DynamicPortfolioApplication.java
+++ src/main/java/com/dynamicportfolio/dynamicportfolio/DynamicPortfolioApplication.java
@@ -2,12 +2,14 @@ package com.dynamicportfolio.dynamicportfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class DynamicPortfolioApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DynamicPortfolioApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DynamicPortfolioApplication.class, args);
+  }
 
 }
